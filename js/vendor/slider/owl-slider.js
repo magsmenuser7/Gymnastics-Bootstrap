4c10252885cd1e1ea39f8538a59dc8b3f705ff78
suresh522 <kajasuresh522@gmail.com>
2024-10-08 20:31:22 +0530

    changes done

---
+++ js/vendor/slider/owl-slider.js
@@ -0,0 +1,59 @@
+// JavaScript Document
+$(document).on('ready', function() {
+	
+	// Header CAROUSEL
+	  var team = $(".owl-carousel");	 //my carousel is user defind
+	  team.owlCarousel({
+		items:1,
+        itemsDesktop:[1000,1],
+        itemsDesktopSmall:[979,1],
+        itemsTablet:[768,1],
+		itemsMobile:[575,1],
+        pagination:true,
+        navigation:false,
+        navigationText:["",""],
+        autoPlay:true,
+		dots:false,
+		  
+	  });
+		
+	 
+	  // Custom Navigation Events
+	  $(".next").click(function(){
+		owl.trigger('owl.next');
+	  })
+	  $(".prev").click(function(){
+		owl.trigger('owl.prev');
+	  })
+	  $(".play").click(function(){
+		owl.trigger('owl.play',500); //owl.play event accept autoPlay speed as second parameter
+	  })
+	  $(".stop").click(function(){
+		owl.trigger('owl.stop');
+	  })
+	  
+});
+
+
+
+// $(document).ready(function(){
+// 	$(".owl-carousel").owlCarousel({
+// 	  loop: true,
+// 	  margin: 10,
+// 	  nav: true,
+// 	  autoplay: true,
+// 	//   autoplayTimeout: 3000, 
+// 	  responsive: {
+// 		0: {
+// 		  items: 1
+// 		},
+// 		600: {
+// 		  items: 1
+// 		},
+// 		1000: {
+// 		  items: 1
+// 		}
+// 	  }
+// 	});
+//   });
+
